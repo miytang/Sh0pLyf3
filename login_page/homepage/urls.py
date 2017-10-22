d642731fe1008ba12d742299baa013b2d9ecf28c
Michael Tang <miytang@ucsc.edu>
2017-10-22 16:45:11 -0700

    Basic login page setup

---
+++ login_page/homepage/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+	url(r'^recipes/$', views.recipes, name='recipes'),
+	url(r'^shopping_list/$', views.shopping_list, name='shopping_list'),
+]
