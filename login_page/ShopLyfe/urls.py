0c9e9406c2e3d77de192298f2edd8dde2452caae
Michael Tang <miytang@ucsc.edu>
2017-10-23 10:22:15 -0700

    Improved login and added user registration.

--- login_page/ShopLyfe/urls.py
+++ login_page/ShopLyfe/urls.py
@@ -17,11 +17,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
+from . import views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='start'),
+    url(r'^$', views.home, name='start'),
 	url(r'^home/', include('homepage.urls')),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
+	url(r'signup/$', views.signup, name='signup'),
     url(r'^admin/', admin.site.urls),
 ]
