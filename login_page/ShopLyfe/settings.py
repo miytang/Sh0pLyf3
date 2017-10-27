592bcd2f427e11ea363ca0c577c48b06b4383455
sportzwiz321 <nmchen@ucsc.edu>
2017-10-26 18:15:59 -0700

    test:

--- login_page/ShopLyfe/settings.py
+++ login_page/ShopLyfe/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipe.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- login_page/ShopLyfe/urls.py
+++ login_page/ShopLyfe/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logged_out.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
+    url(r'^recipe/', include('recipe.urls')),
 ]
