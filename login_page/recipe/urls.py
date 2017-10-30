8c148c4f2314286480d85a2a91806ebb1d4e4710
ButteredNToasted <32777993+ButteredNToasted@users.noreply.github.com>
2017-10-30 11:24:24 -0700

    update added form for recipes

--- login_page/recipe/urls.py
+++ login_page/recipe/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
 	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 	url(r'^(?P<recipe_id>[0-9]+)/test/$', views.test, name='test'),
-        url(r'^recipe/new/$', views.recipe_new, name='recipe_new'),
+        
 ]
