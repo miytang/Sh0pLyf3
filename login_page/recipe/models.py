b6bc8ff8a20117a258d13c358b7f461f1ccc1772
sportzwiz321 <nmchen@ucsc.edu>
2017-10-30 12:08:31 -0700

    Database updated on migration

--- login_page/recipe/models.py
+++ login_page/recipe/models.py
@@ -34,6 +34,10 @@ class Recipe(models.Model):
 	prep_time = models.IntegerField(default=0)
 	cook_time = models.IntegerField(default=0)
 
+	# def __init__(self, recipe_name, recipe_tag, prep_time, cook_time, ingredient_set):
+	# 	r = Recipe(recipe_name = recipe_name, recipe_tag = recipe_tag, prep_time = prep_time, cook_time = cook_time, ingredient_set = ingredient_set)
+	# 	return r
+
 	def __str__(self):
 		return self.recipe_name
 
@@ -49,12 +53,21 @@ class Recipe(models.Model):
 	def details(self):
 		return self.recipe_name, self.recipe_tag, self.prep_time, self.cook_time
 
+	# def addIngredient(self, ingredient_name, ingredient_quantity, quantity_type):
+	# 	self.ingredient_set.create()
+
 class Ingredient(models.Model):
 	recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 	null=True
 	ingredient_name = models.CharField(max_length=200)
 	ingredient_quantity = models.DecimalField(max_digits=5, decimal_places=2)
 	quantity_type = models.CharField(max_length=200)
+
+	# def __init__(self, ingredient_name, ingredient_quantity, quantity_type):
+	# 	self.recipe = recipe
+	# 	self.ingredient_name = ingredient_name
+	# 	self.ingredient_quantity = ingredient_quantity
+	# 	self.quantity_type = quantity_type
 
 	def __str__(self):
 		return self.ingredient_name
