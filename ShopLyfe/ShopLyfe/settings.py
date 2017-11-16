baf841e3a27a236e56192f53f728c4b6621331de
Connor Thurmond <cthurmon@ucsc.edu>
2017-11-15 18:49:40 -0800

    Bootstrap stuff. Hopefully. Definitely working website.

--- ShopLyfe/ShopLyfe/settings.py
+++ ShopLyfe/ShopLyfe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fd&st9&pf7rcr#x-q#8_z3)okibld&c+%bvo%wdwzxo^%1!sb='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True;
 
 ALLOWED_HOSTS = ['54.241.142.236', '127.0.0.1']
 
