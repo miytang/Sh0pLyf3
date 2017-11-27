19198ea46ac6250fe1c3717aa18063438ba2cd18
ButteredNToasted <32777993+ButteredNToasted@users.noreply.github.com>
2017-11-27 11:15:55 -0800

    Add files via upload
    replaced form

--- ShopLyfe/homepage/forms.py
+++ ShopLyfe/homepage/forms.py
@@ -1,22 +1,15 @@
 from django import forms
 from .models import Recipe, Ingredient
-from django.forms.formsets import formset_factory
-from django.forms import inlineformset_factory
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ('recipe_name', 'recipe_tag','prep_time','cook_time', 'my_field')
+        fields = ('recipe_name', 'recipe_tag','prep_time','cook_time')
+
+#IngredientFormSet = inlineformset_factory(Recipe, Ingredient, form=RecipeForm, extra=1)
 
 class IngredientForm(forms.ModelForm):		
 	class Meta:
 		model = Ingredient
-		fields = ('recipe', 'ingredient_name', 'ingredient_quantity', 'quantity_type')
-class UpdateForm(forms.ModelForm):
-    class Meta:
-        model = Recipe
-        fields = ('recipe_name', 'my_field')
-
-
-        
-
+		fields = ('ingredient_name', 'ingredient_quantity', 'quantity_type')
+		
